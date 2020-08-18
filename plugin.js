9496c6bb139303556afec40b9b3dfd8019767f10
Justin <juspears@paypal.com>
2020-08-17 21:34:25 -0700

    fix tests

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
 module.exports = (options, webpack, om) => {
-    const emeth = `${__dirname}`
+    const emeth = `${__dirname}/lib`
     Object.assign(webpack.resolve.alias || (webpack.resolve.alias = {}), {emeth});
 }
--- test/index-test.jsx
+++ test/index-test.jsx
@@ -1,36 +1,35 @@
-import theme, { themeClass, settings } from 'emeth';
-import expect from 'expect';
+import theme, {settings, themeClass} from 'emeth';
+import {expect}                      from 'chai';
 
 const execLoop = c => c();
 describe('theme', function () {
     it('should return in the right order', function () {
         const d  = [
-            theme({ A: { test: 1 } }),
-            theme({ B: { test: 3 } }),
-            theme({ A: { test: 2 } })
+            theme({A: {test: 1}}),
+            theme({B: {test: 3}}),
+            theme({A: {test: 2}})
         ];
-        const tc = themeClass({ displayName: 'A' });
+        const tc = themeClass({displayName: 'A'});
 
-        expect(tc('test')).toEqual('2 1')
+        expect(tc('test')).to.eql('2 1')
         d.forEach(execLoop);
     });
     it('should return unknown class', function () {
         const d       = [
-            theme({ A: { test: 1 } }),
-            theme({ B: { test: 3 } }),
-            theme({ A: { test: 2 } })
+            theme({A: {test: 1}}),
+            theme({B: {test: 3}}),
+            theme({A: {test: 2}})
         ];
         const owarn   = settings.warn;
         const capture = [];
         settings.warn = (...args) => capture.push(args);
 
-        const tc = themeClass({ displayName: 'A' });
-        expect(tc('stuff')).toEqual('stuff');
-        expect(capture.length).toEqual(1);
-        expect(capture[0][0])
-            .toEqual(`could not find a className for '%s' for '%s' `);
-        expect(capture[0][1]).toEqual('A');
-        expect(capture[0][2]).toEqual(['stuff']);
+        const tc = themeClass({displayName: 'A'});
+        expect(tc('stuff')).to.eql('stuff');
+        expect(capture.length).to.eql(1);
+        expect(capture[0][0]).to.eql(`could not find a className for '%s' for '%s' `);
+        expect(capture[0][1]).to.eql('A');
+        expect(capture[0][2]).to.eql('stuff');
 
         d.forEach(execLoop);
         settings.warn = owarn;
